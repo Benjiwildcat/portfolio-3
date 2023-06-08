7e111c97a9078fc9aa87826167578415bbc4582a
benjiwildcat <benjiwildcat@gmail.com>
2023-06-07 23:38:17 -0500

    image placement

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,8 +6,12 @@ export default function About() {
     <div className={styles.about}>
       <h1>About Page</h1>
       <p>
-        Hello, My name is Benjamin Richardson and I'm a full stack developer. Ever since the spring of 2023 I've been developing my skills in a multitude of languages such as JavaScript, CSS, and HTML. Throughout my life I've had an adoration for computer software and the people who put in the work to give us the user interfaces we interact with everyday.
+        Hello, My name is Benjamin Richardson and I'm a full stack developer. Ever since the spring of 2023 I've been developing my skills in a multitude of languages such as JavaScript, CSS, and HTML. Throughout my life I've had an adoration for computer software and the people who put in the work to give us the user interfaces we interact with everyday. I'll admit to not being top shelf or naturally gifted when regards to programming. However, what I can attest to is being diligent and dedicated. I like to see things through and use all possible resources to complete a task.
       </p>
     </div>
   );
 }
+
+{/* <figure class="figure">
+  <img src="..." class="figure-img img-fluid rounded" alt="...">
+</figure> */}
