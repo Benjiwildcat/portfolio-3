3e5db4a318cdda523077f1a67ca260076a3a4cdc
benjiwildcat <benjiwildcat@gmail.com>
2025-09-17 00:30:01 -0500

    css troubleshooting

---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../contact.module.css'
+
+export default function Contact() {
+  return (
+    <div className={styles.contactInfo}>
+      <span className={styles.linkedIn}>LinkedIn: https://www.linkedin.com/in/benjamin-richardson-a0078a113/</span>
+      <span className={styles.email}>Email: benjiwildcat@gmail.com</span>
+      <span className={styles.phone}>Phone: 214-901-1174</span>
+    </div>
+  );
+}
