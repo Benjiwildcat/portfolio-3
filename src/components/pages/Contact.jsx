548b3313191f205b0e399e497b80deb29ab86f6e
benjiwildcat <benjiwildcat@gmail.com>
2025-09-19 13:09:47 -0500

    getting the css right

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,10 +3,10 @@ import styles from '../contact.module.css'
 
 export default function Contact() {
   return (
-    <div className={styles.contactInfo}>
-      <span className={styles.linkedIn}>LinkedIn: https://www.linkedin.com/in/benjamin-richardson-a0078a113/</span>
+    <div className={styles.Contact}>
+      {/* <span className={styles.linkedIn}>LinkedIn: https://www.linkedin.com/in/benjamin-richardson-a0078a113/</span>
       <span className={styles.email}>Email: benjiwildcat@gmail.com</span>
-      <span className={styles.phone}>Phone: 214-901-1174</span>
+      <span className={styles.phone}>Phone: 214-901-1174</span> */}
     </div>
   );
 }
