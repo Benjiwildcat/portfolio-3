b006e8cc5852ed26f17e5886f32e575f571042b9
benjiwildcat <benjiwildcat@gmail.com>
2025-10-30 00:26:30 -0500

    formatting contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,10 +3,31 @@ import styles from '../contact.module.css'
 
 export default function Contact() {
   return (
-    <div className={styles.Contact}>
-      {/* <span className={styles.linkedIn}>LinkedIn: https://www.linkedin.com/in/benjamin-richardson-a0078a113/</span>
-      <span className={styles.email}>Email: benjiwildcat@gmail.com</span>
-      <span className={styles.phone}>Phone: 214-901-1174</span> */}
-    </div>
+
+
+    <><title>Contact Me</title><div className={styles.container}>
+      <div className={styles.header}>
+        <h2>
+          Thank You for looking at my portfolio!
+        </h2>
+      </div>
+
+      <div>
+
+        <div>
+
+          <div className={styles.info}>
+            <a id='linkedin' href='https://www.linkedin.com/in/benjamin-richardson-a0078a113/' target='_blank' rel='noreferrer'>LinkedIn</a>
+            <a href='mailto:benjiwildcat@gmail.com'>Email: benjiwildcat@gmail.com</a>
+            <a href='tel:+12149011174'>Phone: 214-901-1174</a>
+          </div>
+
+        </div>
+
+      </div>
+    </div></>
+
+
+
   );
 }
